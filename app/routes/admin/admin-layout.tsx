5b61819bcccdffc7cd018b698692a3d87a1da1b8
Aroobs-i <iftikhar8995@gmail.com>
2025-09-04 09:50:42 +0500

    routes

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,8 @@
+
+const adminLayout = () => {
+  return (
+    <div className="admin-layout">admin-layout</div>
+  )
+}
+
+export default adminLayout
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,8 @@
+
+const allUsers = () => {
+  return (
+    <div>all-users</div>
+  )
+}
+
+export default allUsers

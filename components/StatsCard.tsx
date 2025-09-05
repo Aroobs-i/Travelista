081ae8523278bc80a866ac688590682fcb0b640f
Aroobs-i <iftikhar8995@gmail.com>
2025-09-05 17:52:34 +0500

    dashboard UI

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,4 +1,4 @@
-import { calculateTrendPercentage } from "~/lib/utils"
+import { calculateTrendPercentage, cn } from "~/lib/utils"
 
 const StatsCard = ({
     headerTitle,
@@ -19,14 +19,22 @@ const StatsCard = ({
                 <div className="flex items-center gap-2">
                     <figure className="flex items-center gap-1">
                         <img src={`/assets/icons/${isDecrement ? 'arrow-down-red.svg': 'arrow-up-green.svg'}`}
-                         alt="stats" 
-                         className=""
+                         alt="arrow" 
+                         className="size-5"
                          />
+                         <figcaption className={cn('text-sm font-medium', isDecrement ? 'text-red-500' :'text-success-700')}>
+                            {Math.round(percentage)}%
+                         </figcaption>
 
                     </figure>
+                    <p className="text-sm font-medium text-gray-100 truncate">
+                        vs last month</p>
 
                 </div>
             </div>
+            <img src={`/assets/icons/${isDecrement ? 'decrement.svg' : 'increment.svg'}`} 
+            className="xl:w-32 w-full h-full md:h-32 xl:h-full"
+            alt="trend graph" />
         </div>
     </h3>
     </article>

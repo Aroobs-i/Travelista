7404384dd369e9512e76443fff27de4dc3374ce3
Aroobs-i <iftikhar8995@gmail.com>
2025-09-09 16:25:56 +0500

    sign in UI

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,6 +1,28 @@
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations" ;
 import { MobileSidebar, NavItems } from "components";
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
+import { getExistingUser, storeUserData } from "~/apppwrite/auth";
+import { account } from "~/apppwrite/client";
+
+export async function clientLoader() {
+  try {
+      const user = await account.get();
+
+      if(!user.$id) return redirect('/sign-in');
+
+      const exisitingUser = await getExistingUser(user.$id);
+
+      if(exisitingUser?.status === 'user'){
+        return redirect('/');
+      }
+      return exisitingUser?.$id ? exisitingUser : await storeUserData();
+      
+  } catch (e) {
+      console.log('Error in ClientLoader',e);
+
+      return redirect('/sign-in');
+  }
+}
 
 const adminLayout = () => {
   return (
@@ -8,7 +30,7 @@ const adminLayout = () => {
       <MobileSidebar/>
       <aside className="w-full max-w-[270px] hidden lg:block">
         <SidebarComponent width={270} enableGestures={false}>
-         <NavItems/>
+         <NavItems />
         </SidebarComponent>
       </aside>
       <aside className="children">

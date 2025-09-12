3c4f9cb4f1d8fb1b4410a4ec802c5efcaee7f54a
Aroobs-i <iftikhar8995@gmail.com>
2025-09-12 11:19:17 +0500

    created Trip UI

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
     route('sign-in','routes/root/sign-in.tsx'),
@@ -7,8 +7,7 @@ export default [
         route('dashboard',  'routes/admin/dashboard.tsx'),
         route('all-users',  'routes/admin/all-users.tsx'),
         route('trips',  'routes/admin/trips.tsx'),
-        route('trips/create',  'routes/admin/create-trip.tsx')
-
-
+        route('trips/create',  'routes/admin/create-trip.tsx'),
+        route('trips/:tripId', 'routes/admin/trip-detail.tsx'),
     ])
 ] satisfies RouteConfig;
